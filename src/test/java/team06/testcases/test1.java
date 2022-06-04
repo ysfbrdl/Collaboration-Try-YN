32aabf2efed88d9ec756cb0c2590e3d81c189234
Yusuf-Cm <102414967+Yusuf-Cm@users.noreply.github.com>
2022-06-04 15:49:36 +0300

    first changing 04/06/22

--- src/test/java/team06/testcases/test1.java
+++ src/test/java/team06/testcases/test1.java
@@ -1,4 +1,9 @@
 package team06.testcases;
 
 public class test1 {
+
+    public static void main(String[] args) {
+        System.out.println("My fist changing");
+        System.out.println("My fist changing at main");
+    }
 }
